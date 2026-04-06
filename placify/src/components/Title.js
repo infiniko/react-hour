de9985060cd7b7ac79cf04c0ee30d0c63ffe8247
infiniko <abhishekacr@gmail.com>
2026-04-07 00:55:06 +0530

    placify react app

---
+++ placify/src/components/Title.js
@@ -0,0 +1,11 @@
+const Title = ({ title, sub }) => {
+  return (
+    <div className="section-title">
+      <h2>
+        {title} <span>{sub}</span>
+      </h2>
+    </div>
+  );
+};
+
+export default Title;
