6f1f3fba554a82456371442497b66f9d68f13a70
infiniko <abhishekacr@gmail.com>
2026-04-11 17:58:13 +0530

    practise=accordian

---
+++ practise-accordian/src/components/Questions.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Question from "./Question";
+
+const Questions = ({ questions }) => {
+  return (
+    <section className="container">
+      <h1>Questions</h1>
+      {questions.map((question) => (
+        <Question key={question.id} {...question} />
+      ))}
+    </section>
+  );
+};
+
+export default Questions;
