5d60f5faa1cc68b802be36b9cdc39ab56dbad35c
infiniko <abhishekacr@gmail.com>
2026-04-23 04:31:01 +0530

    mix-master project

---
+++ mixMaster/src/components/CockTailList.jsx
@@ -0,0 +1,30 @@
+import Wrapper from "../assets/wrappers/CocktailList";
+import CocktailCard from "./CocktailCard";
+
+const CockTailList = ({ drinks }) => {
+  if (!drinks) {
+    return (
+      <h4 style={{ textAlign: "center" }}>No matching cocktails found...</h4>
+    );
+  }
+
+  const formattedDrinks = drinks.map((drink) => {
+    const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = drink;
+    return {
+      id: idDrink,
+      name: strDrink,
+      image: strDrinkThumb,
+      info: strAlcoholic,
+      glass: strGlass,
+    };
+  });
+  return (
+    <Wrapper>
+      {formattedDrinks.map((drink) => {
+        return <CocktailCard key={drink.id} {...drink} />;
+      })}
+    </Wrapper>
+  );
+};
+
+export default CockTailList;
