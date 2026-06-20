79e8ae06ba312ed2b90bdeadabca11185661f43b
infiniko <abhishekacr@gmail.com>
2026-06-20 23:54:13 +0530

    aplyn init

---
+++ aplyn/app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "APLYN",
+  description: "Job application tracking system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        roboto.className,
+        roboto.variable,
+      )}
+    >
+      <body className="min-h-full" suppressHydrationWarning>
+        {children}
+      </body>
+    </html>
+  );
+}
