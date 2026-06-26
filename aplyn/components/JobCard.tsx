04970bfbb666c0944d7aac8889aff758ca3cb125
infiniko <abhishekacr@gmail.com>
2026-06-26 12:25:00 +0530

    job list component added

--- aplyn/components/JobCard.tsx
+++ aplyn/components/JobCard.tsx
@@ -1,7 +1,38 @@
 import { JobType } from "@/utils/types";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
 
-const JobCard = ({ job }: JobType) => {
-  return <div>JobCard</div>;
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import JobInfo from "./JobInfo";
+import JobDeleteBtn from "./JobDeleteBtn";
+
+const JobCard = ({ job }: { job: JobType }) => {
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild>
+          <Link href={`/jobs/${job.id}`}>Edit</Link>
+        </Button>
+        <JobDeleteBtn />
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default JobCard;
